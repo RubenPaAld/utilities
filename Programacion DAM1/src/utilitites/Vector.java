80dd51b22f7e8c139ac9f2eb25030a7e4f0c49a6
rubenpallares <rubenpallares@192.168.1.36>
2017-12-12 20:37:24 +0100

    sdf

--- Programacion DAM1/src/utilitites/Vector.java
+++ Programacion DAM1/src/utilitites/Vector.java
@@ -8,4 +8,13 @@ public class Vector {
 			System.out.println(v[i] + "\t");
 		}
 	}
+	
+	public static void imprimirVectorBidimensionalDeEnteros(int[][] v) {
+		
+		for (int i = 0; i < v.length; i++) {
+			for (int j = 0; j < v[i].length; j++) {
+				System.out.println(v[i][j] + "\t");	
+			}
+		}
+	}
 }
