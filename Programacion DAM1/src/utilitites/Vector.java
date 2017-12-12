21e732d2c25d9bce53a45fd0f36af21ec48dbd6a
rubenpallares <rubenpallares@192.168.1.36>
2017-12-12 20:35:00 +0100

    p`+

---
+++ Programacion DAM1/src/utilitites/Vector.java
@@ -0,0 +1,11 @@
+package utilitites;
+
+public class Vector {
+
+	public static void imprimirVectorDeEnteros(int[] v) {
+		
+		for (int i = 0; i < v.length; i++) {
+			System.out.println(v[i] + "\t");
+		}
+	}
+}
