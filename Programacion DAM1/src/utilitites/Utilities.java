f5895ded01cddfd5a030d053f2c04d1dc39821a5
rubenpallares <rubenpallares@192.168.1.36>
2017-12-13 21:16:51 +0100

    impresion de cualquier dimension

--- Programacion DAM1/src/utilitites/Utilities.java
+++ Programacion DAM1/src/utilitites/Utilities.java
@@ -2,9 +2,32 @@ package utilitites;
 
 public class Utilities {
 
+	public static void imprimirMatriz(Object[] m) {
+
+		for (int i = 0; i < m.length; i++) {
+
+			try {
+				if(m[i].getClass().isArray()) 
+					Utilities.imprimirMatriz((Object[]) m[i]);
+				else
+					System.out.print(m[i] + "\t");
+				
+			}catch(java.lang.NullPointerException e) {
+			
+				System.out.print("null" + "\t");
+			}	
+		}
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			asdfs
+			
+		Integer[][] m = new Integer[8][8];
+		
+		//System.out.println(m[0].getClass().isArray());
+		
+		Utilities.imprimirMatriz(m);
 	}
 
 }
